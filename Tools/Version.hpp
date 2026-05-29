22361cebcc27262f0cfa7691aa514a826b927191
Rétro Dvořák <davidsirait1156@gmail.com>
2026-05-29 21:31:46 +0700

    Improved Tools/Version.hpp

--- Tools/Version.hpp
+++ Tools/Version.hpp
@@ -12,13 +12,13 @@ static constexpr u8 __TOOLS_CPP_VER__   = 10;
 static constexpr u8 __TOOLS_CPP_MAJOR__ = 1;
 static constexpr u8 __TOOLS_CPP_MINOR__ = 0;
 
-static constexpr i8 __TOOLS_INFO_BUFFER__MaxSize = 64;
+static constexpr i8 __TOOLS_INFO_BUFFER_MAX_SIZE = 64;
 
-void PrintVersion() {
-    char __TOOLS_INFO_BUFFER__STRING[__TOOLS_INFO_BUFFER__MaxSize];
+char* Tools_GetVersion() {
+    static char __TOOLS_INFO_BUFFER__STRING[__TOOLS_INFO_BUFFER_MAX_SIZE];
     snprintf(
         __TOOLS_INFO_BUFFER__STRING,
-        __TOOLS_INFO_BUFFER__MaxSize,
+        __TOOLS_INFO_BUFFER_MAX_SIZE,
         #if defined(__clang__) && defined(__clang_major__)
             "Tools.cpp v%i.%i ; this project was/is compiled using Clang %i",
             __TOOLS_CPP_MAJOR__, __TOOLS_CPP_MINOR__, __clang_major__
@@ -30,7 +30,12 @@ void PrintVersion() {
             __TOOLS_CPP_MAJOR__, __TOOLS_CPP_MINOR__
         #endif
     );
-    printf("%s", __TOOLS_INFO_BUFFER__STRING);
+
+    return __TOOLS_INFO_BUFFER__STRING;
+}
+
+void Tools_PrintVersion(){
+    printf("%s", Tools_GetVersion());
 }
 
 #endif
